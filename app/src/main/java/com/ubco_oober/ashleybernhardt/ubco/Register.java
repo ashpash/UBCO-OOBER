817e6fb4086110d4f43a11e13faead39075db72d
Ashley bernhardt <Ashley bernhardt>
2017-02-26 14:13:14 -0800

    Initial commit added basic activities
    -Scroll RSS feed
    -Navigation
    -Register
    -LoginActivity

---
+++ app/src/main/java/com/ubco_oober/ashleybernhardt/ubco/Register.java
@@ -0,0 +1,16 @@
+package com.ubco_oober.ashleybernhardt.ubco;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+    }
+
+    //
+}
