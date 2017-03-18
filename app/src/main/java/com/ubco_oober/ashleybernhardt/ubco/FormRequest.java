b49b33f67e8d93b76704a577d0bf1bcb38ef244c
ashpash <ashleybernhardt@hotmail.com>
2017-03-18 13:31:51 -0700

    almost working

--- app/src/main/java/com/ubco_oober/ashleybernhardt/ubco/FormRequest.java
+++ app/src/main/java/com/ubco_oober/ashleybernhardt/ubco/FormRequest.java
@@ -1,5 +1,7 @@
 package com.ubco_oober.ashleybernhardt.ubco;
 
+import android.util.Log;
+
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.toolbox.StringRequest;
@@ -16,14 +18,18 @@ public class FormRequest extends StringRequest {
     private static final String Form_Request_URL = "https://ubco-oober.000webhostapp.com/Form.php";
     private Map<String, String> params;
 
-    public FormRequest(String studentEmail, String destination, String date, String time, int space, Response.Listener<String> listener) {
+    public FormRequest(String destination, String sDate, String sTime, int space, String studentEmail, Response.Listener<String> listener) {
         super(Request.Method.POST, Form_Request_URL, listener, null);
         params = new HashMap<>() ;
         params.put("studentEmail", studentEmail);
-        params.put("Destination", destination);
-        params.put("Date", date);
-        params.put("Time", time);
-        params.put("Space", space + "");
+        params.put("destination", destination);
+        params.put("sDate", sDate);
+        params.put("sTime", sTime);
+        params.put("space", space + "");
+        Log.d("Destination", destination);
+        Log.d("student email ",studentEmail);
+        Log.d("Destination",sDate);
+        Log.d("Destination",sTime);
 
 
     }
