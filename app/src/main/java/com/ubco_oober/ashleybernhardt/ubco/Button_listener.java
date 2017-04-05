b5c20124fa77a58c819cd41141e3464ec7542fbe
ashpash <ashleybernhardt@hotmail.com>
2017-04-05 12:56:27 -0700

    Revert "Firebase notification"

---
+++ app/src/main/java/com/ubco_oober/ashleybernhardt/ubco/Button_listener.java
@@ -0,0 +1,38 @@
+package com.ubco_oober.ashleybernhardt.ubco;
+
+import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+import static android.content.Intent.FLAG_ACTIVITY_NEW_TASK;
+
+/**
+ * Created by Nick on 2017-03-19.
+ * When the button on the notification bar is pressed, the button leads to the activity containing more info on the ride.
+ * I mean it is a, View more details button.
+ */
+
+public class Button_listener extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        NotificationManager manager = (NotificationManager) context.getSystemService(context.NOTIFICATION_SERVICE);
+        manager.cancel(intent.getExtras().getInt("id"));
+
+        //I CAN SHOW YOU THE WOOORLD
+        //Change the 2nd parameter of myIntent to choose where pressing notif leads you.
+        Intent myIntent = new Intent(context,RSS.class);
+        myIntent.addFlags(FLAG_ACTIVITY_NEW_TASK);
+//        PendingIntent pendingIntent = PendingIntent.getActivity(context,0,myIntent,0);
+
+        context.startActivity(myIntent);
+
+
+
+        Toast.makeText(context, "let's go see some details :D", Toast.LENGTH_SHORT).show();
+
+
+
+    }
+            }
